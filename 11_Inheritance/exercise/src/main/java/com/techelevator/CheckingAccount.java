b248a27cd17fbce4a713e30fb6d922f5d82a1566
Jennifer Brown <jenbrown1983@gmail.com>
2022-03-20 16:53:12 -0500

    inheritance

--- 11_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
+++ 11_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
@@ -1,29 +1,23 @@
 package com.techelevator;
 
-    class CheckingAccount extends BankAccount {
-        public CheckingAccount(String accountHolderName, String accountNumber) {
-            super(accountHolderName,accountNumber);
-        }
-
-        public CheckingAccount(String accountHolderName, String accountNumber, int balance) {
-            super(accountHolderName,accountNumber,balance);
-        }
-
-        public int withdraw(int amountToWithdraw) {
-            int balance = getBalance();
-
-            if(balance-amountToWithdraw > 0) {
-                balance = balance-amountToWithdraw;
-                return balance;
-            }
-
-            if(balance-amountToWithdraw < 0 && balance-amountToWithdraw > -100) {
-                balance = balance-amountToWithdraw-10;
-                return balance;
-            }
-            else
-                return balance;
-        }
+class CheckingAccount extends BankAccount {
+    public CheckingAccount(String accountHolderName, String accountNumber) {
+        super(accountHolderName, accountNumber);
     }
 
+    public CheckingAccount(String accountHolderName, String accountNumber, int balance) {
+        super(accountHolderName, accountNumber, balance);
+    }
+
+    public int withdraw(int amountToWithdraw) {
+        int fee = 10;
+        if (getBalance() - amountToWithdraw > -100) {
+            super.withdraw(amountToWithdraw);
+            if (getBalance() < 0) {
+                super.withdraw(fee);
+            }
+        }
+        return getBalance();
+    }
+}
 
--- 11_Inheritance/exercise/src/main/java/com/techelevator/SavingsAccount.java
+++ 11_Inheritance/exercise/src/main/java/com/techelevator/SavingsAccount.java
@@ -2,23 +2,26 @@ package com.techelevator;
 
 class SavingsAccount extends BankAccount {
     public SavingsAccount(String accountHolderName, String accountNumber) {
-        super(accountHolderName,accountNumber);
+        super(accountHolderName, accountNumber);
     }
-    public SavingsAccount(String accountHolderName, String accountNumber, int balance) {
-        super(accountHolderName,accountNumber,balance);
-    }
-    public int withdraw(int amountToWithdraw) {
-        int balance = getBalance();
 
-        if(balance-amountToWithdraw > 150) {
-            balance = balance-amountToWithdraw;
-            return balance;
-        }
-        if(balance-amountToWithdraw < 150 && balance-amountToWithdraw > 0) {
-            balance = balance-amountToWithdraw-2;
-            return balance;
-        }
-        else
-            return balance;
+    public SavingsAccount(String accountHolderName, String accountNumber, int balance) {
+        super(accountHolderName, accountNumber, balance);
     }
-}
+
+    public int withdraw(int  amountToWithdraw) {
+        int overDraftFee = 2;
+        if (this.getBalance() - amountToWithdraw < 0) {
+            return this.getBalance();
+        }
+        if (this.getBalance() - amountToWithdraw < 150) {
+            super.withdraw(amountToWithdraw + overDraftFee);
+            return this.getBalance();
+        }
+
+        super.withdraw(amountToWithdraw);
+        return getBalance();
+    }
+
+
+    }
