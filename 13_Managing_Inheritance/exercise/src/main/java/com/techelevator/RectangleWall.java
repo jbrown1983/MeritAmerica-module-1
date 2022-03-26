b45e9ee43baba679136f6d6585283d34c6b9b90a
Jennifer Brown <jenbrown1983@gmail.com>
2022-03-26 16:33:51 -0500

    manginginh

---
+++ 13_Managing_Inheritance/exercise/src/main/java/com/techelevator/RectangleWall.java
@@ -0,0 +1,32 @@
+package com.techelevator;
+
+public class RectangleWall extends Wall{
+
+    private int length;
+    private int height;
+
+    public RectangleWall(String name, String color, int length, int height) {
+        super(name, color);
+        this.length = length;
+        this.height = height;
+    }
+    @Override
+    public int getArea() {
+        return length*height;
+    }
+    @Override
+    public String toString() {
+        return super.getName()+" ("+length+"x"+height+") rectangle";
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+
+}
+
---
+++ 13_Managing_Inheritance/exercise/src/main/java/com/techelevator/SquareWall.java
@@ -0,0 +1,22 @@
+package com.techelevator;
+
+public class SquareWall extends RectangleWall {
+
+    private int sideLength;
+
+
+    public SquareWall(String name, String color, int sideLength) {
+        super(name, color, sideLength, sideLength);
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public String toString() {
+        return super.getName() + " (" + sideLength + "x" + sideLength + ") square";
+    }
+
+
+    public int getSideLength() {
+        return sideLength;
+    }
+}
