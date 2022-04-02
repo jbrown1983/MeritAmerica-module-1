aed360a75eef807cd9ddfa6b24886f4c68f308e0
Jennifer Brown <jenbrown1983@gmail.com>
2022-04-02 09:05:32 -0500

    17

--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,6 +1,8 @@
 package com.techelevator;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -10,31 +12,31 @@ public class FizzWriter {
 	{
 		String filename;
 		Scanner sc = new Scanner(System.in);
-
 		System.out.println("Enter the destination file: ");
 		filename = sc.nextLine();
-
 		sc.close();
 
 		try {
+			File fileFizzBuzz = new File (filename);
+			fileFizzBuzz.createNewFile();
 			PrintWriter writer = new PrintWriter(filename);
 
 			for(int n=1;n<=300;n++)
 			{
 				if(n%3 == 0 && n%5 == 0)
 					writer.println("FizzBuzz");
-				else if(n%3 == 0 || (n+"").contains("3"))
+				else if(n%3 == 0)
 					writer.println("Fizz");
-				else if(n%5 == 0 || (n+"").contains("5"))
+				else if(n%5 == 0)
 					writer.println("Buzz");
 				else
 					writer.println(n);
 			}
-
 			writer.close();
-
 		} catch (FileNotFoundException e) {
 			System.out.println("ERROR: Invalid destination file.");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
